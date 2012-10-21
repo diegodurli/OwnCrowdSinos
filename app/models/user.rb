b1922c255532b13f68d43c09a6102759566361a2
Diego Durli <dgodurli@gmail.com>
2012-10-21 16:34:24 -0200

    Deleted index.html and added devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable :recoverable, :rememberable, :trackable, :registerable
-  devise :database_authenticatable, :validatable
+  devise :database_authenticatable, :validatable, :registerable
   attr_accessible :email, :password, :password_confirmation
 end
