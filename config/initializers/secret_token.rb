ff402d82e1d84f91d402a83e65f4e8331e3574ce
Diego Durli <dgodurli@gmail.com>
2012-10-21 15:10:37 -0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OwnCrowdSinos::Application.config.secret_token = 'efccbf2898548dcde32bfa512d18b9a436a537cadbe88a2275f1bb08cd48efed33e891f6c179abca3e3fccc4045f8162b219c994da0fd54c4f72e167aeabc6cf'
